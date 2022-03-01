6051121f80409ff02ad61df4b82f068f99284dd8
kabir <k4birxakya@gmail.com>
2022-03-02 04:39:29 +0545

    MERGED FILES EXCEPT IMAGE SIZES

--- frontned/assets/homejs.js
+++ frontned/assets/homejs.js
@@ -1,13 +1,49 @@
-window.addEventListener("load", function () {
-    setTimeout(open, 5000);
+$(document).ready(function () {
+  if (sessionStorage.getItem('#pop') !== 'true') {
+    setTimeout(open, 8000)
     function open(event) {
       document.querySelector(".wrapper").classList.add("blur");
       document.querySelector(".popup").style.display = "block";
     }
-  });
-  
-  document.querySelector("#close").addEventListener("click", clo);
-  function clo(event) {
-    document.querySelector(".wrapper").classList.remove("blur");
-    document.querySelector(".popup").style.display = "none";
+
+    sessionStorage.setItem('#pop', 'true');
   }
+});
+document.querySelector("#close").addEventListener("click", clo);
+function clo(event) {
+  document.querySelector(".wrapper").classList.remove("blur");
+  document.querySelector(".popup").style.display = "none";
+}
+
+document.querySelector("#mybttn").addEventListener("click", clos);
+function clos(event) {
+  document.querySelector(".slideover").classList.add("animate-slider-over-open"); 
+}
+document.getElementsById("demo1").addEventListener("mouseover", mouseOver);
+function mouseOver() {
+  document.getElementById("demo1").style.borderColor = "red";
+} function mouseOut() {
+  document.getElementById("demo1").style.borderColor = "rgba(136, 133, 133, 0.58)";
+}
+document.getElementsById("demo2").addEventListener("mouseover", mouseover);
+function mouseover() {
+  document.getElementById("demo2").style.borderColor = "red";
+} function mouseout() {
+  document.getElementById("demo2").style.borderColor = "rgba(136, 133, 133, 0.58)";
+}
+document.getElementsById("demo3").addEventListener("mouseover", mouseoveR);
+function mouseoveR() {
+  document.getElementById("demo3").style.borderColor = "red";
+} function mouseouT() {
+  document.getElementById("demo3").style.borderColor = "rgba(136, 133, 133, 0.58)";
+}
+document.getElementsById("demo4").addEventListener("mouseover", Mouseover);
+function Mouseover() {
+  document.getElementById("demo4").style.borderColor = "red";
+} function Mouseout() {
+  document.getElementById("demo4").style.borderColor = "rgba(136, 133, 133, 0.58)";
+}
+
+
+
+
